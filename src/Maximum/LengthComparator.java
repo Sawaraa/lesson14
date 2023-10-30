d5a880cf54d0a90fa1df8a76e2ad4bb013fb405f
Sawaraa <daniellakotlar@gmail.com>
2023-10-30 20:32:04 +0200

    Initial commit

---
+++ src/Maximum/LengthComparator.java
@@ -0,0 +1,10 @@
+package Maximum;
+
+import java.util.Comparator;
+
+public class LengthComparator implements Comparator<Goods> {
+    @Override
+    public int compare(Goods o1, Goods o2) {
+        return o1.getLength() - o2.getLength();
+    }
+}
---
+++ src/Minimum/Human.java
@@ -0,0 +1,12 @@
+package Minimum;
+
+public class Set {
+
+    String name;
+    int age;
+
+    public Set(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
